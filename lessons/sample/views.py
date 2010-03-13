36a263c62fdf7da32efdbbad61fa1930c87feb4e
Mike Koss <mckoss@startpad.org>
2010-03-12 23:19:47 -0800

    Call reqfilter render function

--- lessons/sample/views.py
+++ lessons/sample/views.py
@@ -13,4 +13,4 @@ def add_numbers(req):
     x = int(req.mParams.get('x', 1))
     y = int(req.mParams.get('y', 2))
     req.SetCacheTime(0)
-    return render_to_response('numbers.html', {'x':x, 'y':y, 'z':x+y})
+    return reqfilter.RenderResponse('numbers.html', {'x':x, 'y':y, 'z':x+y})
