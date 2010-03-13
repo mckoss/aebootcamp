119673766cb1fdf25d97a9a92011b40e66ad853c
Johann C. Rocholl <johann@rocholl.net>
2010-03-12 21:11:28 -0800

    Clean and simple guestbook app.

--- lessons/sample/guestbook/urls.py
+++ lessons/sample/guestbook/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('guestbook.views',
     (r'^$', 'list_entries'),
-    (r'^sign/$', 'create_entry'),
 )
--- lessons/sample/guestbook/views.py
+++ lessons/sample/guestbook/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
+from django import forms
 from guestbook.models import Greeting
 
+class GuestbookForm(forms.Form):
+    author = forms.CharField(max_length=40)
+    website = forms.URLField(required=False)
+    content = forms.CharField(widget=forms.Textarea)
+
 def list_entries(request):
+    guestbook_form = GuestbookForm(request.POST or None)
+    if guestbook_form.is_valid():
+        greeting = Greeting(
+            author=guestbook_form.cleaned_data['author'],
+            website=guestbook_form.cleaned_data['website'],
+            content=guestbook_form.cleaned_data['content'])
+        greeting.put()
+        return redirect(list_entries)
+    greetings_list = Greeting.all().order('-date').fetch(10)
     return render_to_response('guestbook/index.html', locals(),
                               context_instance=RequestContext(request))
-
-def create_entry(request):
-    request.POST = request.POST.copy()
-    request.POST['author'] = str(request.user.key())
-    return create_object(request, Greeting, post_save_redirect='/guestbook')
